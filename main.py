ac6ed35b3b8b5f16e0191bfcfb4004794cb08849
Riniel Rodrigo <rinielrodrigo555@gmail.com>
2025-03-26 19:47:24 -0300

    bugfix

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from models import init_db
 from routes import books_bp
 from flask_cors import CORS
 
-# cria a aplicação Flask
+# Cria a aplicação Flask
 app = Flask(__name__)
 CORS(app)
 
-# inicializa o banco de dadospip freeze > requirements.txt
+# Inicializa o banco de dados pip freeze > requirements.txt
 init_db()
 
-# registra o blueprint das rotas
+# Registra o blueprint das rotas
 app.register_blueprint(books_bp)
 
-# inicia o servidor Flask
+# Inicia o servidor Flask
 if __name__ == '__main__':
     app.run(debug=True)
